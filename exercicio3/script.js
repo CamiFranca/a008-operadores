fc7ac8d754596c9f4bda84013fe7fa4728baa16e
CamiFranca <camillaartc@gmail.com>
2022-08-30 16:09:25 -0300

    Tês exercícios de fixação aula 008 JavaS

---
+++ exercicio3/script.js
@@ -0,0 +1,6 @@
+
+ console.log("Resposta: " + (5 > 20 && 5  < 2))
+ console.log("Resposta: " + (5 === 5) || (5=== "5"))
+ console.log("resposta: " + !(20 > 50))
+ console.log("Resposta: " + !(20 > 50)|| (50 > 60))
+
